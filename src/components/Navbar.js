58f8b4d6bd3096d6791d44301b3e4215a51278fe
Leonard Museau <lmuseau@gmail.com>
2020-10-03 18:40:42 -0500

    home page sort of set up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            Saint Robert's First Assembly of God <i className='fab fa-typo3' />
+            Saint Robert's First Assembly of God <i className='fas fa-church' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
@@ -41,13 +41,13 @@ function Navbar() {
               </Link>
             </li>
             <li className='nav-item'>
-              <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
-                Services
+              <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                About
               </Link>
             </li>
             <li className='nav-item'>
-              <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
-                Products
+              <Link to='/events' className='nav-links' onClick={closeMobileMenu}>
+                Events
               </Link>
             </li>
             <li className='nav-item'>
