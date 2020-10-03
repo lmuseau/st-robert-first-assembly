47c0d4469582ef58424b733e3bf7eee6ad78b799
Leonard Museau <lmuseau@gmail.com>
2020-10-03 17:13:39 -0500

    added navbar and button set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
