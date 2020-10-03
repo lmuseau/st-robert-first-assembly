2cc8c9a2ae61585489a2397f0b141858e129d9d1
Leonard Museau <lmuseau@gmail.com>
2020-10-03 16:24:25 -0500

    removed comment again

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() { // hello
+function App() {
   return (
     <div className="App">
       <header className="App-header">
